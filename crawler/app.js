f7a7aebc60de754c689c9ca42f7dba6dffad8268
mattdmayberry <mattdmayberry@gmail.com>
2016-11-04 21:16:19 -0700

    graph function works

--- crawler/app.js
+++ crawler/app.js
@@ -1,10 +1,11 @@
 // app.js
-
+"use strict";
 var app = angular.module('crawler',[]);
 
 app.controller("formCtrl", function ($scope, $http) {
 
-    /************ postData ***************/
+
+    /********** postData *************/
     $scope.postData = function () {
         var data = {
             url: $scope.formData.inputURL,
@@ -33,47 +34,51 @@ app.controller("formCtrl", function ($scope, $http) {
         });
     };
 
+
     /************ getData ***************/
     function getData(data_id) {
+        var crawler_id = data_id;
         var config = {
             headers : {
                 'Content-Type': 'application/json;charset=utf-8;'
             }
         }
-        $http.get('http://138.197.207.83:5000/api/results/' + data_id, config)
+        $http.get('http://138.197.207.83:5000/api/results/3', config)
                 .success(function (data, status, headers, config) {
                 console.log(data.edges[0].id);
-                createGraph(data);   
+                var crawler_data = data;
+                createGraph(crawler_data);   
         });
     };
 
-    /************ graphHelper ***************/
-    function createGraph(data) {
+
+    /********** createGraph *************/
+    function createGraph(crawler_data) {
         var crawler = cytoscape({
-        container: document.getElementById('crawler'),
-        elements: [],
-        style: [
+            container: document.getElementById('crawler'),
+            elements: [],
+            style: [
             {
-                selector: 'node',
-                style: {
+              selector: 'node',
+              style: {
                 shape: 'circle',
                 'background-color': '#1b24d1',
-                label: 'data(id)'
               }
-            }]
+            }],
         });
-
-        for (var i = 0; i < data.edges.length; i++) {
-            addToGraph(data.edges[i], crawler);
-        }
 
         crawler.layout({
             name: 'circle'
         });
+
+        for (var i = 0; i < crawler_data.edges.length; i++) {
+            addToGraph(crawler_data.edges[i], crawler);
+        }
+        //crawler.fit();
     };
 
 
-    /************ addToGraph ***************/
+    /*********** addToGraph *************/
     function addToGraph(linkIn, graphIn) {
       if ((graphIn.getElementById(linkIn.src_url)).length == 0) {
         graphIn.add({
@@ -96,6 +101,25 @@ app.controller("formCtrl", function ($scope, $http) {
         }
       });
     };
+
+    /* function overlap(pos, size) {
+    var overlap = false;
+    cy.nodes(':visible').forEach(function (node) {
+        var npos = node.position();
+        if ((pos.x - size) < npos.x && (pos.x + size) > npos.x && (pos.y - size) < npos.y && (pos.y + size) > npos.y) {
+        overlap = true;
+        return false; //break
+     }
+  });
+  return overlap; */
+}
 });
+
+
+
+
+
+
+
 
 
