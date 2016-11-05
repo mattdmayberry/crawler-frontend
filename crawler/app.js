c0882a01dbe817f2be46545a0c96e71df4325ec3
mattdmayberry <mattdmayberry@gmail.com>
2016-11-04 19:29:11 -0700

    adding GET request

--- crawler/app.js
+++ crawler/app.js
@@ -4,8 +4,8 @@ var app = angular.module('crawler',[]);
 
 app.controller("formCtrl", function ($scope, $http) {
 
-    $scope.sendData = function () {
-       // use $.param jQuery function to serialize data from JSON 
+    /************ postData ***************/
+    $scope.postData = function () {
         var data = {
             url: $scope.formData.inputURL,
             keyword: $scope.formData.keyword,
@@ -20,8 +20,10 @@ app.controller("formCtrl", function ($scope, $http) {
 
         $http.post('http://138.197.207.83:5000/api/requests', data, config)
         .success(function (data, status, headers, config) {
-            //$scope.PostDataResponse = data;
             console.log(data);
+            console.log(data.id);
+            var data_id = data.id;   
+            getData(data_id);
         })
         .error(function (data, status, header, config) {
             $scope.ResponseDetails = "Data: " + data +
@@ -30,4 +32,70 @@ app.controller("formCtrl", function ($scope, $http) {
                 "<hr />config: " + config;
         });
     };
+
+    /************ getData ***************/
+    function getData(data_id) {
+        var config = {
+            headers : {
+                'Content-Type': 'application/json;charset=utf-8;'
+            }
+        }
+        $http.get('http://138.197.207.83:5000/api/results/' + data_id, config)
+                .success(function (data, status, headers, config) {
+                console.log(data.edges[0].id);
+                createGraph(data);   
+        });
+    };
+
+    /************ graphHelper ***************/
+    function createGraph(data) {
+        var crawler = cytoscape({
+        container: document.getElementById('crawler'),
+        elements: [],
+        style: [
+            {
+                selector: 'node',
+                style: {
+                shape: 'circle',
+                'background-color': '#1b24d1',
+                label: 'data(id)'
+              }
+            }]
+        });
+
+        for (var i = 0; i < data.edges.length; i++) {
+            addToGraph(data.edges[i], crawler);
+        }
+
+        crawler.layout({
+            name: 'circle'
+        });
+    };
+
+
+    /************ addToGraph ***************/
+    function addToGraph(linkIn, graphIn) {
+      if ((graphIn.getElementById(linkIn.src_url)).length == 0) {
+        graphIn.add({
+            data: { id: linkIn.src_url }
+          }
+        );
+      }
+      if ((graphIn.getElementById(linkIn.dst_url)).length == 0) {
+        graphIn.add({
+            data: { id: linkIn.dst_url }
+          }
+        );
+      }
+      var source = linkIn.src_url;
+      graphIn.add({
+        data: {
+          id: linkIn.id,
+          source: source,
+          target: (linkIn.dst_url)
+        }
+      });
+    };
 });
+
+
--- crawler/components/graph.js
+++ crawler/components/graph.js
@@ -1,3 +1,172 @@
+"use strict";
+var graph = {
+                "edges": [
+                    {
+                      "create_date": "Wed, 19 Oct 2016 20:32:36 GMT", 
+                      "depth": 0, 
+                      "dst_url": "http://poweredbyorange.com/2015/12/30/jeannie-sullivan-fearless-leader/", 
+                      "had_keyword": false, 
+                      "id": 5824, 
+                      "request": 26, 
+                      "src_url": "http://oregonstate.edu"
+                    }, 
+                    {
+                      "create_date": "Wed, 19 Oct 2016 20:32:36 GMT", 
+                      "depth": 0, 
+                      "dst_url": "http://oregonstate.edu/admissions/static/corvallis/#.Va2SRcZVhkI", 
+                      "had_keyword": false, 
+                      "id": 5825, 
+                      "request": 26, 
+                      "src_url": "http://oregonstate.edu"
+                    }, 
+                    {
+                      "create_date": "Wed, 19 Oct 2016 20:32:36 GMT", 
+                      "depth": 0, 
+                      "dst_url": "http://main.oregonstate.edu/official-web-disclaimer", 
+                      "had_keyword": false, 
+                      "id": 5826, 
+                      "request": 26, 
+                      "src_url": "http://oregonstate.edu"
+                    }, 
+                    {
+                      "create_date": "Wed, 19 Oct 2016 20:32:36 GMT", 
+                      "depth": 0, 
+                      "dst_url": "http://ecampus.oregonstate.edu/", 
+                      "had_keyword": false, 
+                      "id": 5827, 
+                      "request": 26, 
+                      "src_url": "http://oregonstate.edu"
+                    }, 
+                    {
+                      "create_date": "Wed, 19 Oct 2016 20:32:36 GMT", 
+                      "depth": 0, 
+                      "dst_url": "http://oregonstate.edu/faculty/", 
+                      "had_keyword": false, 
+                      "id": 5828, 
+                      "request": 26, 
+                      "src_url": "http://oregonstate.edu"
+                    }, 
+                    {
+                      "create_date": "Wed, 19 Oct 2016 20:32:36 GMT", 
+                      "depth": 0, 
+                      "dst_url": "http://osucascades.edu/feature-story/osu-campus-opens-bend", 
+                      "had_keyword": false, 
+                      "id": 5829, 
+                      "request": 26, 
+                      "src_url": "http://oregonstate.edu"
+                    }, 
+                    {
+                      "create_date": "Wed, 19 Oct 2016 20:32:36 GMT", 
+                      "depth": 0, 
+                      "dst_url": "http://summer.oregonstate.edu/", 
+                      "had_keyword": false, 
+                      "id": 5830, 
+                      "request": 26, 
+                      "src_url": "http://oregonstate.edu"
+                    }, 
+                    {
+                      "create_date": "Wed, 19 Oct 2016 20:32:36 GMT", 
+                      "depth": 0, 
+                      "dst_url": "http://seagrant.oregonstate.edu/", 
+                      "had_keyword": false, 
+                      "id": 5831, 
+                      "request": 26, 
+                      "src_url": "http://oregonstate.edu"
+                    }, 
+                    {
+                      "create_date": "Wed, 19 Oct 2016 20:32:36 GMT", 
+                      "depth": 0, 
+                      "dst_url": "http://oregonstate.edu/current/", 
+                      "had_keyword": false, 
+                      "id": 5832, 
+                      "request": 26, 
+                      "src_url": "http://oregonstate.edu"
+                    }, 
+                    {
+                      "create_date": "Wed, 19 Oct 2016 20:32:36 GMT", 
+                      "depth": 0, 
+                      "dst_url": "http://oregonstate.edu/alumni/", 
+                      "had_keyword": false, 
+                      "id": 5833, 
+                      "request": 26, 
+                      "src_url": "http://oregonstate.edu"
+                    }, 
+                    {
+                      "create_date": "Wed, 19 Oct 2016 20:32:36 GMT", 
+                      "depth": 0, 
+                      "dst_url": "http://oregonstate.edu/ua/ncs/archives/2016/oct/osu-dads-and-moms-weekends-become-family-weekends", 
+                      "had_keyword": false, 
+                      "id": 5834, 
+                      "request": 26, 
+                      "src_url": "http://oregonstate.edu"
+                    }, 
+                    {
+                      "create_date": "Wed, 19 Oct 2016 20:32:36 GMT", 
+                      "depth": 0, 
+                      "dst_url": "http://main.oregonstate.edu/people-search", 
+                      "had_keyword": false, 
+                      "id": 5835, 
+                      "request": 26, 
+                      "src_url": "http://oregonstate.edu"
+                    }, 
+                    {
+                      "create_date": "Wed, 19 Oct 2016 20:32:36 GMT", 
+                      "depth": 0, 
+                      "dst_url": "http://oregonstate.edu/future/", 
+                      "had_keyword": false, 
+                      "id": 5836, 
+                      "request": 26, 
+                      "src_url": "http://oregonstate.edu"
+                    }, 
+                    {
+                      "create_date": "Wed, 19 Oct 2016 20:32:36 GMT", 
+                      "depth": 0, 
+                      "dst_url": "http://pace.oregonstate.edu/", 
+                      "had_keyword": false, 
+                      "id": 5837, 
+                      "request": 26, 
+                      "src_url": "http://oregonstate.edu"
+                    }, 
+                    {
+                      "create_date": "Wed, 19 Oct 2016 20:32:36 GMT", 
+                      "depth": 0, 
+                      "dst_url": "http://oregonstate.edu/ua/ncs/", 
+                      "had_keyword": false, 
+                      "id": 5838, 
+                      "request": 26, 
+                      "src_url": "http://oregonstate.edu"
+                    }, 
+                    {
+                      "create_date": "Wed, 19 Oct 2016 20:32:36 GMT", 
+                      "depth": 0, 
+                      "dst_url": "http://oregonstate.edu/terra/", 
+                      "had_keyword": false, 
+                      "id": 5839, 
+                      "request": 26, 
+                      "src_url": "http://oregonstate.edu"
+                    }, 
+                    {
+                      "create_date": "Wed, 19 Oct 2016 20:32:36 GMT", 
+                      "depth": 0, 
+                      "dst_url": "http://calendar.oregonstate.edu/", 
+                      "had_keyword": false, 
+                      "id": 5840, 
+                      "request": 26, 
+                      "src_url": "http://oregonstate.edu"
+                    }, 
+                    {
+                      "create_date": "Wed, 19 Oct 2016 20:32:36 GMT", 
+                      "depth": 0, 
+                      "dst_url": "http://calendar.oregonstate.edu/event/116673/", 
+                      "had_keyword": false, 
+                      "id": 5841, 
+                      "request": 26, 
+                      "src_url": "http://oregonstate.edu"
+                    }, 
+                  ], 
+            }
+
+
 
 var crawler = cytoscape({
   container: document.getElementById('crawler'),
@@ -38,3 +207,5 @@ for (var i = 0; i < 10; i++) {
 crawler.layout({
     name: 'circle'
 });
+
+
