abf40f829c16128ae99d55bea42a23ce3a1c67cb
mattdmayberry <mattdmayberry@users.noreply.github.com>
2016-12-02 21:25:35 -0800

    Removed http from href links

--- crawler/app.js
+++ crawler/app.js
@@ -177,7 +177,7 @@ app.controller("formCtrl", function ($scope, $http) {
                 console.log( 'tap ' + ele.id());
                 document.getElementById("nodelink").innerHTML = ele.id();
                 var link = document.getElementById("nodelink");
-                link.setAttribute("href", "http://" + ele.id());
+                link.setAttribute("href", ele.id());
             });
         });
     };
